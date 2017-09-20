e4360083c840e6c71852baf6daf0b511246521ad
tpjigar <tp.jigar@gmail.com>
2017-09-20 17:52:41 +0530

    Staff panel updated

--- application/logs/log-2017-09-12.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-09-12 10:20:31 --> Severity: error --> Exception: syntax error, unexpected 'if' (T_IF) D:\XAMPP\htdocs\i5\application\views\backend\admin\country.php 37
